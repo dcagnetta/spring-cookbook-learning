91aae0e64f68b859a6996bc032331e88a6ba7a14
Dillan Cagnetta <dillancagnetta@yahoo.com>
2019-12-03 13:52:21 +0200

    init commit

---
+++ bookpub/src/main/java/co/za/codeboss/host/Application.java
@@ -0,0 +1,12 @@
+package co.za.codeboss.host;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
