014634795dbf99b3114c4500a30327b57741f676
Dillan Cagnetta <dillancagnetta@yahoo.com>
2019-12-03 13:57:44 +0200

    Working sub project

--- bookpub/src/main/java/co/za/codeboss/host/Application.java
+++ bookpub/src/main/java/co/za/codeboss/host/Application.java
@@ -8,5 +8,6 @@ import java.util.Arrays;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Dillan");
     }
 }
